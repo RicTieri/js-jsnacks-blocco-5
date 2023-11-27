1bd13310a660be2070448ed2053c5c9d74c8d71b
RicTieri <tieri.riccardo@gmail.com>
2023-11-27 12:24:10 +0100

    jsnack 2

---
+++ jsnack-2/script.js
@@ -0,0 +1,54 @@
+const people = [
+  {
+    firstName: 'gino',
+    lastName: 'de gino',
+    age: '32'
+  },
+  {
+    firstName: 'gino',
+    lastName: 'de gino',
+    age: '21'
+  },
+  {
+    firstName: 'gino',
+    lastName: 'de gino',
+    age: '12'
+  },
+  {
+    firstName: 'gino',
+    lastName: 'de gino',
+    age: '17'
+  },
+  {
+    firstName: 'gino',
+    lastName: 'de gino',
+    age: '19'
+  },
+  {
+    firstName: 'gino',
+    lastName: 'de gino',
+    age: '23'
+  },
+  {
+    firstName: 'gino',
+    lastName: 'de gino',
+    age: '89'
+  },
+  {
+    firstName: 'gino',
+    lastName: 'de gino',
+    age: '72'
+  },
+]
+
+const patentabili = people.map((person) => {
+  person.fullName = person.firstName + ' ' + person.lastName;
+  if (person.age < 18){
+    person.license = false;
+  } else{
+    person.license = true;
+  }
+  return person.fullName 
+})
+
+console.log(patentabili);
