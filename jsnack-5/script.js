f73f668e426d5ca1ab77249a8dc40cd9c56899b5
RicTieri <tieri.riccardo@gmail.com>
2023-11-28 12:40:35 +0100

    jsnack5-bis

--- jsnack-5/script.js
+++ jsnack-5/script.js
@@ -1,10 +1,10 @@
-Creare un array di oggetti di squadre di volley.
-Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
-Generare numeri random al posto delle nelle proprietà:
-punti fatti e falli subiti
+// Creare un array di oggetti di squadre di volley.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+// Generare numeri random al posto delle nelle proprietà:
+// punti fatti e falli subiti
 
 
-const volleySquad = [{nome:'Bologna', punti:, falli:,},{nome:'Cesena', punti:, falli:,},{nome:'Roma', punti:, falli:,},{nome:'Milano', punti:, falli:,},{nome:'Ortona', punti:, falli:,},{nome:'Canvas', punti:, falli:,}];
+const volleySquad = [{nome:'Bologna', punti: 0, falli: 0,},{nome:'Cesena', punti:0, falli:0,},{nome:'Roma', punti:0, falli:0,},{nome:'Milano', punti:0, falli:0,},{nome:'Ortona', punti:0, falli:0,},{nome:'Canvas', punti:0, falli:0,}];
 
 volleySquad.forEach((element)=>{
   element.punti = getRandom(90);
